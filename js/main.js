d159c365c18db8b8782afb765015407970c2c14d
Ngozi Emeh <zingo1ng@yahoo.com>
2019-08-27 21:58:30 +0100

    pricing table js

--- js/main.js
+++ js/main.js
@@ -12,12 +12,35 @@ const comboArea = document.querySelector(".combooptions");
 // For photo only
 photoButton.addEventListener("click", () => {
     photoArea.classList.toggle("show");
+    if (photoArea.style.display = "flex") {
+        filmArea.style.display = "none";
+        comboArea.style.display = "none";
+    } else {
+        filmArea.style.display = "flex";
+        comboArea.style.display = "flex";
+    };
 });
 // For film only
 filmButton.addEventListener("click", () => {
     filmArea.classList.toggle("show");
+    if (filmArea.style.display = "flex") {
+        photoArea.style.display = "none";
+        comboArea.style.display = "none";
+    } else {
+        photoArea.style.display = "flex";
+        comboArea.style.display = "flex";
+    };
 });
 // For combo
 comboButton.addEventListener("click", () => {
     comboArea.classList.toggle("show");
+    if (comboArea.style.display = "flex") {
+        photoArea.style.display = "none";
+        filmArea.style.display = "none";
+    } else {
+        photoArea.style.display = "flex";
+        filmArea.style.display = "flex";
+    };
+
+
 });
